2d339753e0476dcd2302d555bfb81d9cd8007ac9
TTibbs <terryjw,97@gmail.com>
2023-05-07 17:39:46 +0100

    .

--- script.js
+++ script.js
@@ -5,14 +5,3 @@ showFormButton.addEventListener('click', function() {
   contactForm.style.display = 'block';
   showFormButton.style.display = 'none';
 });
-
-const emailInput = document.querySelector('.email');
-const errorMessage = document.querySelector('.error-message');
-
-emailInput.addEventListener('input', function() {
-  if (emailInput.validity.valid) {
-    errorMessage.style.display = 'none';
-  } else {
-    errorMessage.style.display = 'block';
-  }
-});
